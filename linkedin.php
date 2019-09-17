289945b36b1ea27079111005566bd318d4ae1cbb
webnet <muhommadraahim1@gmail.com>
2019-09-17 17:58:08 +0500

    Update linkedin.php

--- linkedin.php
+++ linkedin.php
@@ -1,1 +1,12 @@
-https://www.linkedin.com/sharing/share-offsite/?url=https://www.google.com
+
+<form action="" method="get">
+Enter Link:<input type="text" name="link" placeholder="link" autocomplete="off" required><br>
+<input type="submit" value="ok">
+</form>
+<?php
+error_reporting(0);
+if (!empty($l = $_GET['link'])) {
+    echo "<a href='https://www.linkedin.com/sharing/share-offsite/?url=".$l."' target='_blank'>Post</a>";    
+}
+//&hashtag=%23AquiVaElHashtag
+?>
